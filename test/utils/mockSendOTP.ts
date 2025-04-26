9fdae2b9a177a33f171a951086c670bb3458cfd3
bonzo4 <alonzo.sabino814@gmail.com>
2025-04-25 20:58:57 -0500

    feat: test setup

---
+++ test/utils/mockSendOTP.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function mockSendOTP(_data: { phoneNumber: string; code: string }) {
+  return;
+}
--- test/utils/setup.ts
+++ test/utils/setup.ts
@@ -1,18 +1,29 @@
 import { treaty } from '@elysiajs/eden';
 import jwt from '@elysiajs/jwt';
-import { schemaBodies, schema } from '@march1-org/db-template';
+import {
+  Auth,
+  createAuthApp,
+  getAuth,
+  validatePhoneNumber,
+} from '@march1-org/auth-api';
 import { config } from 'config';
 import { createApp } from 'createApp';
 import { getCache } from 'lib/cache';
 import { getDb } from 'lib/db';
 
+import { mockSendOTP } from './mockSendOTP';
+
 export async function setup() {
   const db = await getDb();
   const cache = getCache();
+  const authInstance = getAuth({ sendOTP: mockSendOTP, db });
+  const auth = new Auth(authInstance.api, validatePhoneNumber);
+  const authApp = createAuthApp({
+    auth,
+    config,
+  });
   const app = createApp({
-    db,
-    schemaBodies,
-    schema,
+    authApp,
     cache,
   });
 
